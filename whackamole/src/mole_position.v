fdec215e38e3693a2c4122cbd894083339f32608
Tudor Donca <tgdonca@gmail.com>
2019-02-28 19:16:49 -0800

    added mole_position module and testbench

---
+++ whackamole/src/mole_position.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    03:15:09 03/01/2019 
+// Design Name: 
+// Module Name:    mole_position 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module mole_position(
+    );
+
+
+endmodule
---
+++ whackamole/src/tb/mole_position_tb.v
@@ -0,0 +1,46 @@
+`timescale 1ns / 1ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   03:16:00 03/01/2019
+// Design Name:   mole_position
+// Module Name:   /home/ise/XilinxVM/csm152a/whackamole/src/tb/mole_position_tb.v
+// Project Name:  whackamole
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: mole_position
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module mole_position_tb;
+
+	// Outputs
+	wire ;
+
+	// Instantiate the Unit Under Test (UUT)
+	mole_position uut (
+		.()
+	);
+
+	initial begin
+		// Initialize Inputs
+
+		// Wait 100 ns for global reset to finish
+		#100;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
