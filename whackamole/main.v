2c1164d40c8106f28b53e568fd8a68c56af07700
weiblelevi <36179482+weiblelevi@users.noreply.github.com>
2019-03-07 11:49:00 -0800

    Update main.v

--- whackamole/main.v
+++ whackamole/main.v
@@ -18,7 +18,7 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module main(clk, btnUp, btnDown, btnLeft, btnRight, btnCenter, sw, led);
+module main(clk, btnUp, btnDown, btnLeft, btnRight, btnCenter, sw, led, seg, an);
 
 input clk;
 input btnUp;
@@ -29,6 +29,8 @@ input btnCenter;
 input sw;
 
 output [7:0] led;
+output [7:0] seg;
+output [3:0] an;
 
 wire guess_now;
 wire [2:0] user_guess;
@@ -39,6 +41,14 @@ wire guess_wrong;
 wire [2:0] mole_pos;
 wire mole_change;
 wire [7:0] score;
+wire clk_fast;
+wire clk_blink;
+wire clk_pixel;
+
+wire [3:0] digit_4 = score % 10;
+wire [3:0] digit_3 = (score / 10) % 10;
+wire [3:0] digit_2 = (score / 100) % 10;
+wire [3:0] digit_1 = (score / 1000) % 10;
 
 user_input user_input(.clk(clk), .btnUp(btnUp), .btnDown(btnDown), .btnLeft(btnLeft), .btnRight(btnRight), .btnCenter(btnCenter), .sw(sw), .guess_now(guess_now), 
 								.user_guess(user_guess), .rst(rst), .eval_now(eval_now));
@@ -49,5 +59,9 @@ score_evaluation score_evaluation(.clk(clk), .user_guess(user_guess), .mole_pos(
 								.guess_correct(guess_correct), .guess_wrong(guess_wrong), .guess_now(guess_now));
 								
 led_display led_display (.i_clk(clk), .i_user_guess(user_guess), .i_mole_position(mole_pos), .i_user_right(guess_correct), .i_user_wrong(guess_wrong), .leds(led));
+
+seven_display seven_display(.clk_fast(clk_fast), .clk_blink(clk_blink), .digit_1(digit_1), .digit_2(digit_2), .digit_3(digit_3), .digit_4(digit_4), .seg(seg), .an(an));
+
+clocks clocks(.rst(rst), .master_clk(clk), .clk_fast(clk_fast), .clk_blink(clk_blink), .clk_pixel(clk_pixel));
 
 endmodule
